8b1e176fc644828f377e73a2b89a38ab2ec336e8
ahmedelhdad <106435040+ahmedelhdad@users.noreply.github.com>
2024-03-13 16:33:51 +0200

    Update PageProducts.jsx

--- src/Components/Website/PageProducts.jsx
+++ src/Components/Website/PageProducts.jsx
@@ -104,7 +104,7 @@ const PageProducts = () => {
         </div>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="container py-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {handlerProducts} 
           </div>
         )}
